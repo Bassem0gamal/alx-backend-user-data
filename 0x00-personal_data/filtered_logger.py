dd07a9b4fb685de8cdd67473bf7f66d411545752
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-09-13 05:48:50 +0300

    Task 0 : Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Filter_datum """
+    for field in fields:
+        message = re.sub(f"{field}=[^{separator}]*",
+                         f"{field}={redaction}", message)
+    return message
