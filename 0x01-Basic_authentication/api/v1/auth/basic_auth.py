b2fac9a723adfdcdd5b4410aa4633db9ed622e4b
Bassem Gamal <bassemgamalosman12@gmail.com>
2024-09-22 00:02:33 +0300

    Fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,8 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ BasicAuth class """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """ Returns the Base64 part of the Authorization header """
         if not authorization_header or\
            not isinstance(authorization_header, str) or\
